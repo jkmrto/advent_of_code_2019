5ef0ca73b3f3bac9c699ae5d1d4e5313a83f0eca
jkmrto <juanka1331@gmail.com>
2019-12-10 19:44:44 +0100

    day10 part2 done

--- day10/main.go
+++ day10/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
+	"sort"
 	"strings"
 
 	"github.com/advent_of_code_2019/utils"
@@ -57,7 +59,7 @@ type angle struct {
 	X, Y int
 }
 
-func buildRadar(board [][]string, stationPosY int, stationPosX int) int {
+func buildRadar(board [][]string, stationPosY int, stationPosX int) map[angle][]int {
 	radar := map[angle][]int{}
 	for y := 0; y < len(board); y++ {
 		for x := 0; x < len(board[0]); x++ {
@@ -75,7 +77,7 @@ func buildRadar(board [][]string, stationPosY int, stationPosX int) int {
 	// fmt.Printf("%+v", radar)
 	// fmt.Printf("\n%+v\n", len(keys(radar)))
 
-	return len(keys(radar))
+	return radar
 
 }
 
@@ -100,10 +102,11 @@ func part1(board [][]string) (maxVisibleAsteroids int, maxVisibleAsteroidsPosX i
 	for y := 0; y < len(board); y++ {
 		for x := 0; x < len(board[0]); x++ {
 			if board[y][x] == "#" {
-				visibleAsteroids := buildRadar(board, y, x)
+				radar := buildRadar(board, y, x)
+				visibleAsteroids := len(keys(radar))
 				if visibleAsteroids > maxVisibleAsteroids {
 					maxVisibleAsteroidsPosX = x
-					maxVisibleAsteroidsPosX = y
+					maxVisibleAsteroidsPosY = y
 					maxVisibleAsteroids = visibleAsteroids
 				}
 			}
@@ -112,11 +115,103 @@ func part1(board [][]string) (maxVisibleAsteroids int, maxVisibleAsteroidsPosX i
 	return
 }
 
+type asteroidsRelativeToStation struct {
+	degrees          float64
+	ang              angle
+	stepsToAsteroids []int
+}
+
+func buildRadarWithDegrees(board [][]string, posX int, posY int) []asteroidsRelativeToStation {
+	radar := buildRadar(board, posY, posX)
+	radarWithDegrees := []asteroidsRelativeToStation{}
+	for ang, stepsToAsteroids := range radar {
+		sort.Ints(stepsToAsteroids)
+		asteroidsRelativePosition := asteroidsRelativeToStation{
+			degrees:          CoordinatesToDegrees(ang.X, ang.Y),
+			ang:              ang,
+			stepsToAsteroids: stepsToAsteroids,
+		}
+		radarWithDegrees = append(radarWithDegrees, asteroidsRelativePosition)
+
+	}
+
+	sort.Slice(radarWithDegrees, func(i, j int) bool {
+		return radarWithDegrees[i].degrees < radarWithDegrees[j].degrees
+	})
+
+	return radarWithDegrees
+}
+
+func buildPositionWithRadar(posToStation asteroidsRelativeToStation, index int, stationPosX int, stationPosY int) (posX int, posY int) {
+	// fmt.Printf("%d, %d", posToStation.ang.X*posToStation.stepsToAsteroids[index], posToStation.ang.Y*posToStation.stepsToAsteroids[index])
+	posX = stationPosX + posToStation.ang.X*posToStation.stepsToAsteroids[index]
+	posY = stationPosY + posToStation.ang.Y*posToStation.stepsToAsteroids[index]
+	return
+}
+
 func main() {
 	// board := LoadBoard("./day10/example1")
+	// board := LoadBoard("./day10/example2")
+	// board := LoadBoard("./day10/example3")
+
 	board := LoadBoard("./day10/input")
 
 	maxVisible, posX, posY := part1(board)
 	fmt.Printf("Part2 Solution: (%d, %d): %d\n", posX, posY, maxVisible)
 
+	radarWithDegrees := buildRadarWithDegrees(board, posX, posY)
+
+	i := 0
+	counter := 1
+	for {
+		asteroidX, asteroidY := buildPositionWithRadar(radarWithDegrees[i], 0, posX, posY)
+		fmt.Printf("\nCounter: %d, Position: (%d, %d) Removing element: %+v", counter, asteroidX, asteroidY, radarWithDegrees[i])
+		if len(radarWithDegrees[i].stepsToAsteroids) > 1 {
+			radarWithDegrees[i].stepsToAsteroids = radarWithDegrees[i].stepsToAsteroids[1:]
+			i++
+		} else {
+			radarWithDegrees = remove(radarWithDegrees, i)
+		}
+
+		if i == len(radarWithDegrees) {
+			fmt.Printf("\n>>>>>> Starting Other Round <<<<<<")
+			i = 0
+		}
+
+		if len(radarWithDegrees) == 0 {
+			break
+		}
+		counter++
+	}
+
+}
+
+func remove(slice []asteroidsRelativeToStation, s int) []asteroidsRelativeToStation {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func CoordinatesToDegrees(x int, y int) float64 {
+	if y < 0 && x > 0 {
+		return math.Atan(float64(utils.Abs(x))/float64(utils.Abs(y))) * 180 / math.Pi
+	} else if y > 0 && x > 0 {
+		return 180 - math.Atan(float64(utils.Abs(x))/float64(utils.Abs(y)))*180/math.Pi
+	} else if y > 0 && x < 0 { // 3 cuadrante
+		return 180 + math.Atan(float64(utils.Abs(x))/float64(utils.Abs(y)))*180/math.Pi
+	} else if y < 0 && x < 0 { // 4 cuadrante
+		return 360 - math.Atan(float64(utils.Abs(x))/float64(utils.Abs(y)))*180/math.Pi
+	} else { // Special cases
+		if y == -1 && x == 0 {
+			return float64(0)
+		} else if y == 0 && x == 1 {
+			return float64(90)
+		} else if y == 1 && x == 0 {
+			return float64(180)
+		} else if y == 0 && x == -1 {
+			return float64(270)
+		} else {
+			log.Panic("Something weird has happened")
+		}
+	}
+
+	return float64(-1)
 }
--- day10/main_test.go
+++ day10/main_test.go
@@ -6,14 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPart1(t *testing.T) {
-	board := LoadBoard("./input")
-	maxVisible, _, _ := part1(board)
-	assert.Equal(t, 284, maxVisible, "FUCK")
-}
+func TestCoordinatesToDegrees(t *testing.T) {
 
-func TestExample1(t *testing.T) {
-	board := LoadBoard("./example1")
-	maxVisible, _, _ := part1(board)
-	assert.Equal(t, 8, maxVisible, "FUCK")
+	// Typical case 1 to 1
+	assert.Equal(t, 45.0, CoordinatesToDegrees(1, -1), "FUCK")
+	assert.Equal(t, 135.0, CoordinatesToDegrees(1, 1), "FUCK")
+	assert.Equal(t, 225.0, CoordinatesToDegrees(-1, 1), "FUCK")
+	assert.Equal(t, 315.0, CoordinatesToDegrees(-1, -1), "FUCK")
+
+	// // Typical case 1 to 3
+	// assert.Equal(t, 18.43494882292201, CoordinatesToDegrees(1, 3), "FUCK")
+	// assert.Equal(t, 161.56505117707798, CoordinatesToDegrees(1, -3), "FUCK")
+	// assert.Equal(t, 198.43494882292202, CoordinatesToDegrees(-1, -3), "FUCK")
+	// assert.Equal(t, 341.565051177078, CoordinatesToDegrees(-1, 3), "FUCK")
+
+	// Special cases
+	assert.Equal(t, 0.0, CoordinatesToDegrees(0, -1), "FUCK")
+	assert.Equal(t, 90.0, CoordinatesToDegrees(1, 0), "FUCK")
+	assert.Equal(t, 180.0, CoordinatesToDegrees(0, 1), "FUCK")
+	assert.Equal(t, 270.0, CoordinatesToDegrees(-1, 0), "FUCK")
+
 }
